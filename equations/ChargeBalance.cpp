9cc1fdc8c3b02182e2e887dd186205410d712bd6
Karthikeyan Chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2026-09-07 22:32:09 +0100

    Forming K Q C but coeffs are yet to be passed

--- equations/ChargeBalance.cpp
+++ equations/ChargeBalance.cpp
@@ -2,37 +2,28 @@
 #include "equations/ChargeBalance.hpp"
 
 void
-ChargeBalance::Update(const Coefficient & j)
+ChargeBalance::Update()
 {
-  // Source term.
-  Vector source_vec({/* NE */ -AN /* length scaling */ * (LNE / NNE * NX),
-                     /* SEP */ 0.,
-                     /* PE */ -AP /* length scaling */ * (LPE / NPE * NX)});
-
-  PWConstCoefficient source_part(source_vec);
-  ProductCoefficient source(source_part, const_cast<Coefficient &>(j));
-
-  // Effective conductivity (does not account for electrode filler).
-  Vector sigma_vec({/* NE */ (1 - EPS_N) * SIGN /* length scaling */ / (LNE / NNE * NX),
-                    /* SEP */ 0.,
-                    /* PE */ (1 - EPS_P) * SIGP /* length scaling */ / (LPE / NPE * NX)});
-
-  PWConstCoefficient sigma(sigma_vec);
-
   if (!K)
   {
     K = new ParBilinearForm(&fespace);
-    K->AddDomainIntegrator(new DiffusionIntegrator(sigma));
+    K->AddDomainIntegrator(new DiffusionIntegrator);
     K->Assemble();
-    K->FormSystemMatrix(ess_tdof_list, Kmat);
+  }
+
+  if (!C)
+  {
+    C = new ParMixedBilinearForm(&fespace, fespace2);
+    C->AddDomainIntegrator(new MixedScalarMassIntegrator);
+    C->Assemble();
   }
 
   if (!Q)
   {
     Q = new ParLinearForm(&fespace);
-    Q->AddDomainIntegrator(new DomainLFIntegrator(source));
+    mfem::ConstantCoefficient one(1.0);
+    Q->AddDomainIntegrator(new DomainLFIntegrator(one));
   }
   Q->Assemble();
   Q->ParallelAssemble(b);
-  b.SetSubVector(ess_tdof_list, 0.0);
 }
--- equations/ChargeBalance.hpp
+++ equations/ChargeBalance.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
-#include "mfem.hpp"
 #include "equations/Equation.hpp"
-
-using namespace mfem;
 
 class ChargeBalance : public Equation
 {
 public:
-  ChargeBalance(ParFiniteElementSpace & f) : Equation(f)
-  {
-    f.GetEssentialTrueDofs(Array<int>({1, 1}), ess_tdof_list);
-  }
-  virtual void Update(const Coefficient & j) override;
+  using Equation::Equation;
+
+  virtual void Update();
 };
