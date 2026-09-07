315f4093ba62761d46e7ae2f403b663b32374e85
Karthikeyan Chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2026-09-07 22:30:18 +0100

    Equation now allows for mixed systems

--- equations/Equation.hpp
+++ equations/Equation.hpp
@@ -10,32 +10,43 @@ class Equation
 {
 protected:
   ParFiniteElementSpace & fespace;
+  ParFiniteElementSpace * fespace2 = nullptr;
 
   Array<int> ess_tdof_list; // this list remains empty for pure Neumann b.c.
 
   ParBilinearForm * M = nullptr;
   ParBilinearForm * K = nullptr;
   ParLinearForm * Q = nullptr;
+  ParMixedBilinearForm * C = nullptr;
 
   HypreParMatrix Mmat;
   HypreParMatrix Kmat;
+  HypreParMatrix Cmat;
 
   Vector b; // auxiliary vector
 
 public:
-  Equation(ParFiniteElementSpace & f) : fespace(f), b(f.GetTrueVSize()) {};
+  Equation(ParFiniteElementSpace & f,
+           ParFiniteElementSpace * f2 = nullptr)
+    : fespace(f),
+      fespace2(f2),
+      b(f.GetTrueVSize())
+  {}
 
   const HypreParMatrix & GetM() const { return Mmat; };
   const HypreParMatrix & GetK() const { return Kmat; };
+  const HypreParMatrix & GetC() const { return Cmat; };
   const Vector & GetZ() const { return b; };
 
   virtual void Update(const Coefficient & j) {}
-  virtual void Update(const GridFunctionCoefficient & u, const Coefficient & j) {}
+  virtual void Update(const GridFunctionCoefficient & u,
+                      const Coefficient & j) {}
 
   virtual ~Equation()
   {
     delete M;
     delete K;
     delete Q;
+    delete C;
   }
 };
